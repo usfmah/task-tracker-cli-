b7cbda3ac1c732702a17099d6c72d25750c48153
Soofa <youssefjoo2255@gmail.com>
2026-08-23 18:03:33 +0300

    chore: update gitignore

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ import express, { type Express, type Request, type Response } from 'express';
 const app: Express = express();
 
 
-
 app.listen(3000, () => {
   console.log(`Example app listening on port ${3000}`);
 });
