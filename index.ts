a3556e67deb5242a60956141ee51a6dc17a914d1
Soofa <youssefjoo2255@gmail.com>
2026-08-24 00:44:03 +0300

    refactor: restructure project architecture

--- index.ts
+++
@@ -1,8 +0,0 @@
-import express, { type Express, type Request, type Response } from 'express';
-
-const app: Express = express();
-
-
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`);
-});
