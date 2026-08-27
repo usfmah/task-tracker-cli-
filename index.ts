648a52767bd212d153504114dd0a57028f94c2b8
Soofa <youssefjoo2255@gmail.com>
2026-08-27 04:27:00 +0300

    feat: wire full CLI dispatcher in index.ts
    - handle add, update, delete, mark-in-progress, mark-done, list
    - validate missing args, unknown commands

--- index.ts
+++ index.ts
@@ -1,12 +1,61 @@
 import { parsedArgs } from './src/commandExtractor.ts';
-import { add } from './src/taskOperation.ts'
+import { add, update, del, list, markInProgress, markDone } from './src/taskOperation.ts';
 
-const { command, description } = parsedArgs;
+const { command, id, description } = parsedArgs;
 
-if (command === 'add' && description) {
-  add(description);
-} else if (command === 'add') {
-  console.log('Description is missed');
+switch (command) {
+    case 'add': {
+        if (!description) {
+            console.log('Description is missed');
+        } else {
+            await add(description);
+        }
+        break;
+    }
+    case 'update': {
+        if (!id || !description) {
+            console.log('Usage: update <id> <description>');
+        } else {
+            await update(id, description);
+        }
+        break;
+    }
+    case 'delete': {
+        if (!id) {
+            console.log('Usage: delete <id>');
+        } else {
+            await del(id);
+        }
+        break;
+    }
+    case 'mark-in-progress': {
+        if (!id) {
+            console.log('Usage: mark-in-progress <id>');
+        } else {
+            await markInProgress(id);
+        }
+        break;
+    }
+    case 'mark-done': {
+        if (!id) {
+            console.log('Usage: mark-done <id>');
+        } else {
+            await markDone(id);
+        }
+        break;
+    }
+    case 'list': {
+        await list(id);
+        break;
+    }
+    case undefined: {
+        console.log('Usage: task-cli <command> [args]');
+        console.log('Commands: add, update, delete, mark-in-progress, mark-done, list [status]');
+        break;
+    }
+    default: {
+        console.log(`Unknown command "${command}"`);
+        console.log('Commands: add, update, delete, mark-in-progress, mark-done, list [status]');
+        break;
+    }
 }
-
-
